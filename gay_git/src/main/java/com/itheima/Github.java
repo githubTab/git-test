585223b904f22d5488a4a18c7231453657c94f26
git <git@itheima.com>
2019-02-25 10:54:40 +0800

    first 上传

---
+++ gay_git/src/main/java/com/itheima/Github.java
@@ -0,0 +1,10 @@
+package com.itheima;
+
+public class Github {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
